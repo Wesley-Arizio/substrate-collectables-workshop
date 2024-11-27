25f9a26d219600199dd57a6d1037441a68bc2520
wesley arizio <wesleyarizio26@gmail.com>
2024-11-26 22:35:24 -0300

    finish storage basics chapter

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,26 @@ pub use pallet::*;
 
 #[frame::pallet(dev_mode)]
 pub mod pallet {
+
 	use super::*;
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(core::marker::PhantomData<T>);
 
+	#[pallet::error]
+	pub enum Error<T> {
+		TooManyKitties,
+		DuplicateKitty,
+	}
+
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
+
+	#[pallet::storage]
+	pub(super) type CountForKitties<T: Config> =
+		StorageValue<Value = u32, QueryKind = ValueQuery, OnEmpty = GetDefault>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -24,14 +35,15 @@ pub mod pallet {
 		Created { owner: T::AccountId },
 	}
 
-	#[pallet::error]
-	pub enum Error<T> {}
+	#[pallet::storage]
+	pub(super) type Kitties<T: Config> = StorageMap<Key = [u8; 32], Value = ()>;
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		pub fn create_kitty(origin: OriginFor<T>) -> DispatchResult {
 			let who = ensure_signed(origin)?;
-			Self::mint(who)?;
+			let dna = [0u8; 32];
+			Self::mint(who, dna)?;
 			Ok(())
 		}
 	}
